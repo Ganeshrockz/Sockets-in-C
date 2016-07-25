55f83f04e52accec852b473fb9f26fe1b06aca4b
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-07-26 00:12:19 +0530

    server done

--- http/http_server.c
+++ http/http_server.c
@@ -1,53 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#define MAX 20
-
-struct request
-{
-        char method[MAX];
-        char url[MAX];
-        char version[MAX];
-        char connection[MAX];
-        char user_agent[MAX];
-        char accept[MAX];
-        char accept_lang[MAX];
-};
-
-struct response
-{
-	char version[MAX];
-	char status_msg[MAX];
-	char date[MAX];
-	char server[MAX];
-	char last_mod[MAX];
-	char content_type[MAX];
-	char data[MAX];
-};
+#define BUFF 265
 
 int main(void)
 {
 	int network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(3000);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(3000);
+	server.sin_addr.s_addr = INADDR_ANY;
 
-	bind(network_socket, (struct sockaddr*)&server_address, sizeof(server_address));	
-
+	bind(network_socket, (struct sockaddr*)&server, sizeof(server));
 	listen(network_socket, 2);
-
-	while(1)
-	{
-		int client_socket = accept(network_socket, NULL, NULL);
-		recv(client_socket, &sample, sizeof(sample), 0);
-		printf("a is %d\n", sample.a);
-		printf("buff is %s\n", sample.buff);
-		close(client_socket);
+	while(1){
+		int client = accept(network_socket, NULL, NULL);
+		FILE *fp = fopen("index.html", "r");
+		char res[BUFF*5] = "HTTP/1.1 GET OK\r\n\n";
+		char data[BUFF*4] = "\0";
+		fscanf(fp, "%[^EOF]", data);
+		strcat(res, data);
+		send(client, res, BUFF*5, 0);
+		close(client);
 	}
 	return 0;
 }
