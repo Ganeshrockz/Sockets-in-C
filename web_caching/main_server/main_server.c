3744a493e6c8035be76feeb507a3df78a2b381f7
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-08-15 20:13:04 +0530

    server is made generic

--- web_caching/main_server/main_server.c
+++ web_caching/main_server/main_server.c
@@ -8,33 +8,67 @@
 #include <pthread.h>
 #define LISTENERS	10
 #define BUFFSIZE	1024
+#define FILESIZE	128
 
-int sendRes(int client)
+int sendRes(int client, char* filename, char* response, int status)
 {
-	char filename[] = "index.html";
-	char response[BUFFSIZE] = "HTTP/1.1 GET OK\r\n\r\n";
-	char data[BUFFSIZE] 	= "\0";
-	FILE *FP = fopen(filename, "r");
-	fscanf(FP, "%[^EOF]", data);
+	char data[BUFFSIZE] = "\0";
+	if(status == 0) {
+		strcpy(data, "<html><head><title>Error</title></head><body><h1>404 Error</h1></body></html>");
+	} else {
+		FILE *FP = fopen(filename, "r");
+		fscanf(FP, "%[^EOF]", data);
+		fclose(FP);
+	}
 	strcat(response, data);
-	fclose(FP);
-	send(client, response, sizeof response, 0);
+	send(client, response, strlen(response), 0);
 	return 0;
 }
 
-int recvReq(int client)
+int recvReq(int client, char* filename)
 {
 	char request[BUFFSIZE] = "\0";
 	recv(client, request, sizeof request, 0);
 	puts(request);
+
+	if(request[0]=='G' && request[1]=='E' && request[2]=='T') {
+		if(request[5] == ' ') {
+			strcpy(filename, "index.html");
+		} else {
+			int i = 5;
+			while(request[i]!=' ') {
+				filename[i-5] = request[i];
+				i++;
+			}
+			filename[i-5] = 0;
+		}	
+	}
 	return 0;
 }	
+
+int check_file(char* filename, char* response)
+{
+	if(access(filename, 0) == 0) {
+		strcpy(response, "HTTP/1.1 GET OK\r\n\r\n");
+		return 1;
+	}
+	else {
+		strcpy(response, "HTTP/1.1 404 File not found\r\n\r\n");
+		return 0;
+	}
+}
 
 void* client_work(void* arg)
 {
 	int* client = (int*) arg;
-	recvReq(*client);
-	sendRes(*client);
+
+	char filename[FILESIZE];
+	recvReq(*client, filename);
+
+	char response[BUFFSIZE];
+	int status = check_file(filename, response);
+
+	sendRes(*client, filename, response, status);
 }
 
 int main(int number, char* port[])
@@ -71,7 +105,7 @@ int main(int number, char* port[])
 
 	listen(server_socket, LISTENERS);
 	printf("=> Listening to %d Listeners\n", LISTENERS);
-	
+
 	while(1) {
 		int client = accept(server_socket, NULL, NULL);
 		if(client != -1) {
@@ -80,6 +114,7 @@ int main(int number, char* port[])
 			pthread_create(&tid, NULL, client_work, &client);
 			pthread_join(tid, NULL);
 			close(client);
+			printf("Client closed\n\n");
 		}
 	}
 	close(server_socket);	
